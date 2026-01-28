bf4989ecfc50b0473216587291c18c988ddbd6cf
Folke Ishii <ishii@student.chalmers.se>
2026-01-28 10:22:39 +0100

    Add pauli X,Y,Z gates

--- src/gate.rs
+++ src/gate.rs
@@ -1,6 +1,44 @@
-use nalgebra::{Complex, DMatrix, DVector};
+use nalgebra::{Complex, DMatrix};
 
 pub struct Gate {
     pub matrix: DMatrix<Complex<f32>>,
     pub target: Vec<usize>,
+}
+
+impl Gate {
+    const PAULI_X_DATA: [Complex<f32>; 4] = [
+        Complex::new(0.0, 0.0), Complex::new(1.0, 0.0),
+        Complex::new(1.0, 0.0), Complex::new(0.0, 0.0),
+    ];
+
+    const PAULI_Y_DATA: [Complex<f32>; 4] = [
+        Complex::new(0.0, 0.0), Complex::new(0.0, -1.0),
+        Complex::new(0.0, 1.0), Complex::new(0.0, 0.0),
+    ];
+
+    const PAULI_Z_DATA: [Complex<f32>; 4] = [
+        Complex::new(1.0, 0.0), Complex::new(0.0, 0.0),
+        Complex::new(0.0, 0.0), Complex::new(-1.0, 0.0),
+    ];
+
+    pub fn x(target: usize) -> Gate {
+        Self {
+            matrix: DMatrix::from_row_slice(2, 2, &Self::PAULI_X_DATA),
+            target: vec![target],
+        }
+    }
+
+    pub fn y(target: usize) -> Gate {
+        Self {
+            matrix: DMatrix::from_row_slice(2, 2, &Self::PAULI_Y_DATA),
+            target: vec![target],
+        }
+    }
+
+    pub fn z(target: usize) -> Gate {
+        Self {
+            matrix: DMatrix::from_row_slice(2, 2, &Self::PAULI_Z_DATA),
+            target: vec![target],
+        }
+    }
 }
