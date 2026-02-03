256ad5c76aac559b857d9acd0a9b069e0128a7f0
Sebastian Sandstig <sebastian.sandstig@gmail.com>
2026-02-03 11:46:22 +0100

    Changed general structure

--- src/simulator.rs
+++ src/simulator.rs
@@ -1,1 +1,11 @@
-pub struct SimpleSimulator {}
+use nalgebra::{Complex, DVector};
+
+use crate::Circuit;
+
+pub trait SimpleSimulator: Sized {
+    type E: std::error::Error;
+
+    fn build(circuit: Circuit) -> Result<Self, Self::E>;
+    fn run(&self) -> usize;
+    fn final_state(&self) -> DVector<Complex<f32>>;
+}
