7b9f7a0e5fe19c0f926530cc7cc44716185386e8
Albin Andersson <albin.andersson041@gmail.com>
2026-02-05 11:52:16 +0100

    begin controlled gate implementation

--- src/simulator.rs
+++ src/simulator.rs
@@ -1,5 +1,5 @@
-use nalgebra::{Complex, DVector};
-use crate::Circuit;
+use nalgebra::{Complex, DMatrix, DVector, DefaultAllocator, DimName, Matrix, Vector, allocator::Allocator};
+use crate::{Circuit, Instruction};
 use rand::prelude::*;
 
 pub struct Qubit {
@@ -9,8 +9,11 @@ pub struct Qubit {
 
 impl Default for Qubit {
     // Default qubit is a guaranteed 0 with no phase AKA |0>
-    fn default() -> Self { 
-        {Complex::new(1, 0), Complex::new(0)} 
+    fn default() -> Qubit {
+        Self {
+            alfa: Complex::new(1.0, 0.0),
+            beta: Complex::new(0.0, 0.0)
+        } 
     } 
 }
 
@@ -22,7 +25,7 @@ pub struct SimpleSimulator {
      * a vector of each qubit for now. It should be possible to map
      * between them...
     */
-    qubits: Vec<Qubit>,
+    state_vector: Vec<Complex<f32>>,
     
     /* For now, the *simple* simulator can hold an entire circuit,
      * maybe there is a better way? 
@@ -40,32 +43,71 @@ impl Collapsed {
 
 impl SimpleSimulator {
 
-    fn state_vector() -> Vec<Complex<f32>> {
-        // TODO: Calculate state vector
+    fn controls_active(i: usize, controls: &[usize]) -> bool {
+        controls.iter().all(|&c| ((i >> c) & 1) == 1)
     }
 
-    // Changes the state of the simulator according to its quantum circuit.
-    fn perform_all_gates(self: mut Self) {
-        for i: Instruction in self.circuit {
-            // TODO: Apply gates, maybe crash on measurements for now?
-        }
+    fn is_block_base(i: usize, targets: &[usize]) -> bool {
+        targets.iter().all(|&t| ((i >> t) & 1) == 0)
     }
 
-    fn run(&mut self) -> Collapsed {
-        self.perform_all_gates(); 
-        let mut rng = rand::rng();
-        let r = rng.random() // Random f64 in [0, 1)
-        let last_probability_end = 0f64;
-        for (bitstring, component) in self.calculate_state().enumerate() {
-            let probability: f32 = component.pow(2).norm();
-            let this_probability_end = last_probability_end + probability;
-            if last_probability_end <= r && r < this_probability_end {
-                return bitstring
+    fn block_indices(base: usize, targets: &[usize]) -> Vec<usize> {
+        let k = targets.len();
+        let mut indices = Vec::with_capacity(1 << k);
+
+        for mask in 0..(1 << k) {
+            let mut idx = base;
+            // We embed the bits of mask into the corresponding target qubit positions
+            for (j, &target_bit_pos) in targets.iter().enumerate() {
+                // Take the j:th bit of the mask and place it at the correct position
+                // of the target bit
+                idx |= ((mask >> j) & 1) << target_bit_pos;
             }
-            last_probability_end = this_probability_end;
+            indices.push(idx);
         }
-        // If we did not collapse to any bitstring at all at this point,
-        // something is very wrong!
-        panic!("Tried to collapse state, but did not land on any bitstring!");
+
+        indices
+    
+    }
+
+    fn apply_controlled_gate(
+        &mut self,
+        controls: Vec<usize>,
+        targets: Vec<usize>,
+        u: DMatrix<Complex<f32>>
+    )
+    {
+        // State vector is length 2^n , n=num qubits
+        for i in 0..self.state_vector.len() {
+            if !SimpleSimulator::is_block_base(i, &targets) {
+                continue;
+            }
+
+            if !SimpleSimulator::controls_active(i, &controls) {
+                continue;
+            }
+
+            let indices = SimpleSimulator::block_indices(i, &targets);
+
+            // Read amplitudes
+            let mut v = Vec::with_capacity(indices.len());
+            for &idx in &indices {
+                v.push(self.state_vector[idx]);
+            }
+
+            let v = DVector::from_iterator(
+                indices.len(),
+                indices.iter().map(|&idx| self.state_vector[idx])
+            );
+
+            // Apply gate matrix, assumes u matches size of v
+            let v2 = &u * &v;
+
+            // Write updated amplitudes back
+            for (j, &idx) in indices.iter().enumerate() {
+                self.state_vector[idx] = v2[j];
+            }
+        }
+
     }
 }
