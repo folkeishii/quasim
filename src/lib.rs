caa7757bb06d31f256541efe32436f8d50e2bb1d
ottoSvalin <144454085+ottoSvalin@users.noreply.github.com>
2026-02-10 11:37:44 +0100

    21 simpledebugsim (#27)
    * trait for DebugSimulator
    
    * removed debugsim trait
    
    * Working concept but messy
    
    Will clean up later
    
    * Clean-up and tests
    
    * Futher clean-up
    
    * Error enum
    
    * merge with main

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,10 @@ mod circuit;
 mod instruction;
 mod simulator;
 mod simple_simulator;
+mod debug_simulator;
 
 pub use circuit::*;
 pub use instruction::*;
 pub use simulator::*;
 pub use simple_simulator::*;
+pub use debug_simulator::*;
