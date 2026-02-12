fcd39797d28cf19087b286f574fa7bdb1f0f5870
Virtuo1 <37273266+Virtuo1@users.noreply.github.com>
2026-02-12 17:06:42 +0100

    Fredkin swap gate (#28)

--- src/instruction.rs
+++ src/instruction.rs
@@ -7,6 +7,8 @@ pub enum Instruction {
     Z(usize),
     Y(usize),
     H(usize),
+    SWAP(usize, usize),
+    CSWAP(usize, usize, usize), // Fredkin gate
 }
 
 impl Instruction {
@@ -33,23 +35,34 @@ impl Instruction {
         Complex::new(FRAC_1_SQRT_2, 0.0), Complex::new(-FRAC_1_SQRT_2, 0.0),
     ];
 
+    #[rustfmt::skip]
+    pub const SWAP_DATA: [Complex<f32>; 16] = [
+        Complex::ONE, Complex::ZERO, Complex::ZERO, Complex::ZERO,
+        Complex::ZERO, Complex::ZERO, Complex::ONE, Complex::ZERO,
+        Complex::ZERO, Complex::ONE, Complex::ZERO, Complex::ZERO,
+        Complex::ZERO, Complex::ZERO, Complex::ZERO, Complex::ONE
+    ];
+
     // Can add an argument specifying if we should include full matrix including for the control bits of the gate
     // Right now just return the basic gate so for a CNOT gate we just return X data
     pub fn get_matrix(&self) -> DMatrix<Complex<f32>> {
-        let data = match self {
-            Instruction::X(_) => &Self::PAULI_X_DATA,
-            Instruction::Y(_) => &Self::PAULI_Y_DATA,
-            Instruction::Z(_) => &Self::PAULI_Z_DATA,
-            Instruction::H(_) => &Self::HADAMARD_DATA,
-            Instruction::CNOT(_, _) => &Self::PAULI_X_DATA,
+        let (dim, data): (usize, &[Complex<f32>]) = match self {
+            Instruction::X(_) => (2, &Self::PAULI_X_DATA),
+            Instruction::Y(_) => (2, &Self::PAULI_Y_DATA),
+            Instruction::Z(_) => (2, &Self::PAULI_Z_DATA),
+            Instruction::H(_) => (2, &Self::HADAMARD_DATA),
+            Instruction::CNOT(_, _) => (2, &Self::PAULI_X_DATA),
+            Instruction::SWAP(_, _) => (4, &Self::SWAP_DATA),
+            Instruction::CSWAP(_, _, _) => (4, &Self::SWAP_DATA),
         };
 
-        return DMatrix::from_row_slice(2, 2, data);
+        return DMatrix::from_row_slice(dim, dim, data);
     }
 
     pub fn get_controls(&self) -> Vec<usize> {
         match self {
-            Instruction::CNOT(c, _) => vec![*c],
+            Instruction::CNOT(c, _)
+            | Instruction::CSWAP(c, _, _) => vec![*c],
             _ => vec![],
         }
     }
@@ -61,6 +74,8 @@ impl Instruction {
             | Instruction::Y(t)
             | Instruction::Z(t)
             | Instruction::H(t) => vec![*t],
+            Instruction::SWAP(t1, t2)
+            | Instruction::CSWAP(_, t1, t2) => vec![*t1, *t2],
         }
     }
 }
