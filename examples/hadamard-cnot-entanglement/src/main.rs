f286036410362f55db8f0470fa58e5770c0cb04a
Emil Karlsson <emil.axel.karlsson@gmail.com>
2026-02-19 12:11:44 +0100

    WIP: DAG traversal - re-exports

--- examples/hadamard-cnot-entanglement/src/main.rs
+++ examples/hadamard-cnot-entanglement/src/main.rs
@@ -1,9 +1,15 @@
-use quasim;
-fn main() {
-    let circuit = quasim::Circuit::default()
-        .hadamard(0)
-        .cnot(0, 1)
-        .measure(None);
+use quasim::{self, SimpleSimpleSimulator, SimpleSimulator};
 
-    todo!()
+fn main() {
+    // let circuit = quasim::Circuit::new(2).hadamard(0).cnot(0, 1);
+    let circuit = match quasim::Circuit::from_qasm_file("src/circuit.qasm") {
+        Ok(circuit) => circuit,
+        Err(e) => panic!("Error reading QASM file: {}", e),
+    };
+    let sim = match SimpleSimpleSimulator::build(circuit) {
+        Ok(sim) => sim,
+        Err(e) => panic!("Error building simulator: {}", e),
+    };
+
+    println!("\nResult: {:#04b}", sim.run());
 }
--- src/circuit.rs
+++ src/circuit.rs
@@ -1,3 +1,7 @@
+use std::fs::read_to_string;
+
+use oq3_syntax::{SourceFile, SyntaxNode, ast::AstNode};
+
 use crate::instruction::Instruction;
 
 #[derive(Debug, Clone)]
@@ -6,12 +10,59 @@ pub struct Circuit {
     pub n_qubits: usize,
 }
 
+type QuasimParseError = String;
+
 impl Circuit {
     pub fn new(n_qubits: usize) -> Self {
         Circuit {
             instructions: Vec::<Instruction>::default(),
             n_qubits,
         }
+    }
+
+    fn print_tree(file: SourceFile) {
+        for item in file.syntax().descendants() {
+            println!("{:?}", item);
+        }
+    }
+
+    fn print_children(file: SourceFile) {
+        for item in file.syntax().children() {
+            println!("{:?}", item);
+        }
+    }
+
+    pub fn from_qasm_file(file_name: &str) -> Result<Self, QuasimParseError> {
+        let file_string = read_to_string(file_name).map_err(|e| e.to_string())?;
+        let parsed_source = SourceFile::parse(&file_string);
+        let parse_tree: SourceFile = parsed_source.tree();
+        println!(
+            "Found {} statements",
+            parse_tree.statements().collect::<Vec<_>>().len()
+        );
+        let syntax_errors = parsed_source.errors();
+        println!(
+            "Found {} parse errors:\n{:?}\n",
+            syntax_errors.len(),
+            syntax_errors
+        );
+        Self::instructions_from_syntax_tree(parse_tree.syntax());
+        // Self::print_children(parse_tree);
+        return Ok(Circuit::new(2));
+    }
+
+    fn instructions_from_syntax_tree(node: &SyntaxNode) -> Vec<Instruction> {
+        use oq3_syntax::SyntaxKind::*;
+        let recurse = Self::instructions_from_syntax_tree;
+        let mut instructions = Vec::<Instruction>::default();
+        node.children().for_each(|child| match child.kind() {
+            EXPR_STMT => {
+                println!("Expression statement: {:?}", child);
+                recurse(&child);
+            }
+            _ => println!("Unknown instruction: {:?}", child),
+        });
+        return instructions;
     }
 
     pub fn x(mut self, target: usize) -> Self {
