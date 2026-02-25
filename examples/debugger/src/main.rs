7db25814ffd296e43c0716350acb5535c5cf9b23
Sebastian Sandstig <sebastian.sandstig@gmail.com>
2026-02-25 16:55:34 +0100

    fix example

--- examples/debugger/src/main.rs
+++ examples/debugger/src/main.rs
@@ -1,11 +1,12 @@
-use std::{io, thread::sleep, time::Duration};
+use std::io;
 
 use quasim::circuit::Circuit;
+use quasim::debug_simulator::DebugSimulator;
 use quasim::debug_terminal::DebugTerminal;
 
 fn main() -> io::Result<()> {
     let circuit = Circuit::new(3).hadamard(0).cnot(0, 2).cnot(2, 1);
-    let mut term = DebugTerminal::new(circuit).expect("Test could not build debug terminal");
+    let mut term = DebugTerminal::<DebugSimulator>::new(circuit).expect("Test could not build debug terminal");
 
     term.run()
 }
