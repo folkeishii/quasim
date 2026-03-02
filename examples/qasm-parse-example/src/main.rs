574badba11f8332959b749a1e28793c109735b38
Emil Karlsson <emil.axel.karlsson@gmail.com>
2026-03-02 16:12:29 +0100

    Working qasm parse example

--- examples/qasm-parse-example/src/main.rs
+++ examples/qasm-parse-example/src/main.rs
@@ -1,12 +1,16 @@
-use quasim::{self, circuit::Circuit, simple_simulator::SimpleSimpleSimulator, simulator::SimpleSimulator};
+use quasim::{
+    self,
+    circuit::Circuit,
+    simulator::{BuildSimulator, RunnableSimulator},
+    sv_simulator::SVSimulator,
+};
 
 fn main() {
-    // let circuit = quasim::Circuit::new(2).hadamard(0).cnot(0, 1);
     let circuit = match Circuit::from_qasm_file("src/circuit.qasm") {
         Ok(circuit) => circuit,
         Err(e) => panic!("Error reading QASM file: {}", e),
     };
-    let sim = match SimpleSimpleSimulator::build(circuit) {
+    let sim = match SVSimulator::build(circuit) {
         Ok(sim) => sim,
         Err(e) => panic!("Error building simulator: {}", e),
     };
