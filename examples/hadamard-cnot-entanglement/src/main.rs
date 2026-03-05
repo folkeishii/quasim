e301749734c404fcd43a61f116a1550650472fbb
Emil Karlsson <emil.axel.karlsson@gmail.com>
2026-03-05 10:04:40 +0100

    Fixed non-QASM hadamard-cnot example

--- examples/hadamard-cnot-entanglement/src/main.rs
+++ examples/hadamard-cnot-entanglement/src/main.rs
@@ -1,9 +1,17 @@
-use quasim;
-fn main() {
-    let circuit = quasim::Circuit::default()
-        .hadamard(0)
-        .cnot(0, 1)
-        .measure(None);
+use quasim::{
+    circuit::Circuit,
+    simulator::{BuildSimulator, RunnableSimulator},
+    sv_simulator::SVSimulator,
+};
+extern crate pretty_env_logger;
 
-    todo!()
+fn main() {
+    pretty_env_logger::init();
+    let circuit = Circuit::new(2).hadamard(0).cnot(0, 1);
+    let sim = match SVSimulator::build(circuit) {
+        Ok(sim) => sim,
+        Err(e) => panic!("Error building simulator: {}", e),
+    };
+
+    println!("\nResult: {:#04b}", sim.run());
 }
