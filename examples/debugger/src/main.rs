f73166f42b4695d79fa6bba2a6180a5bf7de79ec
Sebastian Sandstig <sebastian.sandstig@gmail.com>
2026-03-09 17:14:41 +0100

    checks

--- examples/debugger/src/main.rs
+++ examples/debugger/src/main.rs
@@ -1,9 +1,9 @@
 use std::io;
 
 use quasim::circuit::Circuit;
-use quasim::debug_simulator::DebugSimulator;
+// use quasim::debug_simulator::DebugSimulator;
 use quasim::debug_terminal::DebugTerminal;
-// use quasim::sv_simulator::SVSimulatorDebugger;
+use quasim::sv_simulator::SVSimulatorDebugger;
 
 fn main() -> io::Result<()> {
     let circuit = Circuit::new(3).h(0).cx(&[0], 2).cx(&[2], 1);
--- examples/sub-circuits/src/main.rs
+++ examples/sub-circuits/src/main.rs
@@ -1,60 +1,60 @@
-use quasim::{
-    circuit::Circuit,
-    debug_terminal::DebugTerminal,
-    simulator::BuildSimulator,
-    sv_simulator::{SVSimulator, SVSimulatorDebugger},
-};
+// use quasim::{
+//     circuit::Circuit,
+//     debug_terminal::DebugTerminal,
+//     simulator::BuildSimulator,
+//     sv_simulator::{SVSimulator, SVSimulatorDebugger},
+// };
 
 fn main() {
-    let sub_circuit = Circuit::new(2).hadamard(0).cnot(0, 1);
-    let circuit = Circuit::new(3)
-        .new_sub_circuit("sub", sub_circuit)
-        .hadamard(0)
-        .cnot(0, 1)
-        .cnot(1, 2)
-        .call("sub", 0)
-        .call("sub", 1)
-        .call("sub", 0)
-        .call("sub", 1);
+    // let sub_circuit = Circuit::new(2).hadamard(0).cnot(0, 1);
+    // let circuit = Circuit::new(3)
+    //     .new_sub_circuit("sub", sub_circuit)
+    //     .hadamard(0)
+    //     .cnot(0, 1)
+    //     .cnot(1, 2)
+    //     .call("sub", 0)
+    //     .call("sub", 1)
+    //     .call("sub", 0)
+    //     .call("sub", 1);
 
-    let svsim = SVSimulator::build(circuit).unwrap();
-    let debugger = svsim.attach_debugger();
-    let mut term = DebugTerminal::<SVSimulatorDebugger>::from_simulator(debugger);
-    term.run().unwrap();
+    // let svsim = SVSimulator::build(circuit).unwrap();
+    // let debugger = svsim.attach_debugger();
+    // let mut term = DebugTerminal::<SVSimulatorDebugger>::from_simulator(debugger);
+    // term.run().unwrap();
 
-    let sub_circuit = Circuit::new(2).hadamard(0).breakpoint().cnot(0, 1);
-    let circuit = Circuit::new(3)
-        .new_sub_circuit("sub", sub_circuit)
-        .hadamard(0)
-        .cnot(0, 1)
-        .cnot(1, 2)
-        .call("sub", 0)
-        .call("sub", 1)
-        .call("sub", 0)
-        .call("sub", 1);
+    // let sub_circuit = Circuit::new(2).hadamard(0).breakpoint().cnot(0, 1);
+    // let circuit = Circuit::new(3)
+    //     .new_sub_circuit("sub", sub_circuit)
+    //     .hadamard(0)
+    //     .cnot(0, 1)
+    //     .cnot(1, 2)
+    //     .call("sub", 0)
+    //     .call("sub", 1)
+    //     .call("sub", 0)
+    //     .call("sub", 1);
 
-    let svsim = SVSimulator::build(circuit).unwrap();
-    let debugger = svsim.attach_debugger();
-    let mut term = DebugTerminal::<SVSimulatorDebugger>::from_simulator(debugger);
-    term.run().unwrap();
+    // let svsim = SVSimulator::build(circuit).unwrap();
+    // let debugger = svsim.attach_debugger();
+    // let mut term = DebugTerminal::<SVSimulatorDebugger>::from_simulator(debugger);
+    // term.run().unwrap();
 
-    let sub_circuit = Circuit::new(2).hadamard(0).cnot(0, 1);
-    let circuit = Circuit::new(3)
-        .new_sub_circuit("sub", sub_circuit)
-        .hadamard(0)
-        .cnot(0, 1)
-        .cnot(1, 2)
-        .call("sub", 0)
-        .breakpoint()
-        .call("sub", 1)
-        .breakpoint()
-        .call("sub", 0)
-        .breakpoint()
-        .call("sub", 1)
-        .breakpoint();
+    // let sub_circuit = Circuit::new(2).hadamard(0).cnot(0, 1);
+    // let circuit = Circuit::new(3)
+    //     .new_sub_circuit("sub", sub_circuit)
+    //     .hadamard(0)
+    //     .cnot(0, 1)
+    //     .cnot(1, 2)
+    //     .call("sub", 0)
+    //     .breakpoint()
+    //     .call("sub", 1)
+    //     .breakpoint()
+    //     .call("sub", 0)
+    //     .breakpoint()
+    //     .call("sub", 1)
+    //     .breakpoint();
 
-    let svsim = SVSimulator::build(circuit).unwrap();
-    let debugger = svsim.attach_debugger();
-    let mut term = DebugTerminal::<SVSimulatorDebugger>::from_simulator(debugger);
-    term.run().unwrap()
+    // let svsim = SVSimulator::build(circuit).unwrap();
+    // let debugger = svsim.attach_debugger();
+    // let mut term = DebugTerminal::<SVSimulatorDebugger>::from_simulator(debugger);
+    // term.run().unwrap()
 }
