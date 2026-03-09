7005fc8df0488688bcc2d99a22d7ba44be6335ee
Jonas Lutschke <lutschke@chalmers.se>
2026-03-09 17:30:14 +0100

    Algorithm now works

--- examples/grovers/src/main.rs
+++ examples/grovers/src/main.rs
@@ -3,57 +3,72 @@ use quasim::expr_dsl::Value;
 use quasim::simulator::{BuildSimulator, DebuggableSimulator, HybridSimulator};
 use quasim::sv_simulator::SVSimulatorDebugger;
 
-const BITS: usize = 2; // Number of input bits
 const PI: f64 = 3.14; // Used in calculating the number of iterations for Grover's algorithm
 
-const FUNC: [usize; BITS] = [0, 1]; // f(x) written as b_x,b_(x-1),...,b_0
+fn check_quantum(func: &[usize]) -> bool {
+    let bits: usize = func.len();
+    let n = 1 << bits;
+    let mut circuit = Circuit::new(bits).new_reg("res");
 
-fn check_quantum(function: [usize; BITS]) -> bool {
-    let n = 1 << BITS-1;
-    let mut circuit = Circuit::new(BITS).new_reg("res");
-
-    for i in 0..BITS {
-        circuit = circuit.hadamard(i);
+    for i in 0..bits {
+        circuit = circuit.h(i);
     }
 
-    for _i in 0..(PI/4.0*((n as f64).sqrt())) as usize {
-        
+    let iterations = (PI/4.0*((n as f64).sqrt())).floor() as usize;
+
+    for _i in 0..iterations {
+
+
+        // Controlbits
+        let c_array = (0..bits-1).collect::<Vec<usize>>();
+
         // Oracle
-        for (j, &bit) in function.iter().enumerate() {
-            if bit == 1{
-                circuit = circuit.z(j)
+        for (j, &bit) in func.iter().rev().enumerate() {
+            if bit == 0{
+                circuit = circuit.x(j);
+            }
+        }
+        circuit = circuit.h(bits-1);
+
+        circuit = circuit.cx(&c_array, bits-1);
+
+        circuit = circuit.h(bits-1);
+
+        for (j, &bit) in func.iter().rev().enumerate() {
+            if bit == 0{
+                circuit = circuit.x(j);
             }
         }
 
         // Diffusion
-        for i in 0..BITS-1 {
-            circuit = circuit.hadamard(i);
+        for i in 0..bits {
+            circuit = circuit.h(i);
+            circuit = circuit.x(i);
         }
-        circuit = circuit.z(BITS-1);
 
-        circuit = circuit.cnot(0, 1);
+        circuit = circuit.h(bits-1);
 
-        for i in 0..BITS-1 {
-            circuit = circuit.hadamard(i);
+        circuit = circuit.cx(&c_array, bits-1);
+
+        circuit = circuit.h(bits-1); 
+
+        for i in 0..bits {
+            circuit = circuit.x(i);
+            circuit = circuit.h(i);
         }
-        circuit = circuit.z(BITS-1);
+        
     }
-
-   
-    circuit = circuit.measure(&(0..BITS).collect::<Vec<usize>>(), "res");
+    
+    circuit = circuit.measure(&(0..bits).collect::<Vec<usize>>(), "res");
 
     let mut sim = SVSimulatorDebugger::build(circuit).unwrap();
     sim.continue_until(None);
-    println!("State: {}",sim.current_state());
 
-    let fun_res: usize = FUNC.iter().rev().enumerate().map(|(i, &b)| b << i).sum();
+    let fun_res: usize = func.iter().rev().enumerate().map(|(i, &b)| b << i).sum();
 
     match sim.register("res"){
         Value::Int(x) => {
             let res = x == fun_res as i32;
-            if !res {
-                panic!("expected {}, got {}", fun_res, x);
-            }
             res
         },
         Value::Float(_) => {
@@ -66,5 +81,17 @@ fn check_quantum(function: [usize; BITS]) -> bool {
 }
 
 fn main(){
-    println!("{}", check_quantum(FUNC));
+
+    let func: &[usize] = &[1, 1]; // f(x) written as b_x,b_(x-1),...,b_0
+
+    let iter = 100;
+    let mut true_count = 0;
+
+    for _ in 0..iter {
+        if check_quantum(func) {
+            true_count += 1;
+        }
+    }
+
+    println!("True count: {}", true_count);
 }
