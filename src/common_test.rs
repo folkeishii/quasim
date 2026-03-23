358cc1f8300e4c4313ad491828515d9dfadec7f2
Sebastian Sandstig <sebastian.sandstig@gmail.com>
2026-03-23 09:45:19 +0100

    readded and renamed almost_grovers

--- src/common_test.rs
+++ src/common_test.rs
@@ -16,18 +16,18 @@ pub fn almost_grovers<
     // Keep for sub circuits
     return;
     const N: usize = 2;
-    // let sub = Circuit::new(N)
-    //     // Step 1
-    //     .h(0)
-    //     .z(1)
-    //     // Step 2
-    //     .cx(&[0], 1)
-    //     // Step 3
-    //     .h(0)
-    //     .z(1);
+    let sub = Circuit::new(N)
+        // Step 1
+        .h(0)
+        .z(1)
+        // Step 2
+        .cx(&[0], 1)
+        // Step 3
+        .h(0)
+        .z(1);
 
     let mut circuit = Circuit::new(2 * N)
-        // .new_sub_circuit("sub", sub)
+        .new_sub_circuit("sub", sub)
         // Init
         .h(0)
         .h(1)
@@ -37,8 +37,8 @@ pub fn almost_grovers<
     const L: usize = 5; //(std::f64::consts::PI * 2f64.sqrt() / 4f64).floor() as usize;
 
     for _ in 0..L {
-        // circuit = circuit.call("sub", 0);
-        // circuit = circuit.call("sub", 2);
+        circuit = circuit.call("sub", 0);
+        circuit = circuit.call("sub", 2);
     }
 
     let mut sim = D::build(circuit.into()).expect("Could not build simulator");
