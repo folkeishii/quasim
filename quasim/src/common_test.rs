5e04f5edc60f617c32bff1388240fe43bc7cadff
Albin Andersson <37273266+Virtuo1@users.noreply.github.com>
2026-04-02 14:51:54 +0200

    Register refactor & Separate Expr -> BitExpr, BoolExpr (#182)
    * Register and expression refactor, added variable size of registers
    
    * Register value u64 -> usize
    
    * introduced workspace
    
    * Only shared dependencies in workspace
    
    * Fix examples and Into Bit/BoolExpr
    
    * Adressed comments, fixed test
    
    * Update CI check

--- quasim/src/common_test.rs
+++ quasim/src/common_test.rs
@@ -5,7 +5,7 @@ use nalgebra::{Complex, DVector, dvector};
 use crate::{
     cart,
     circuit::{Circuit, HybridCircuit},
-    expr_dsl::{Value, expr_helpers::r},
+    expr_dsl::expr_helpers::r,
     ext::equal_to_matrix_c,
     simulator::{BuildSimulator, DebuggableSimulator, HybridSimulator, StoredCircuitSimulator},
 };
@@ -152,10 +152,10 @@ pub fn hybrid_test<
     D: BuildSimulator<HybridCircuit> + DebuggableSimulator + StoredCircuitSimulator,
 >() {
     let circuit = Circuit::new(4)
-        .new_reg("r0")
-        .new_reg("r1")
-        .new_reg("r2")
-        .new_reg("r3")
+        .new_reg("r0", 1)
+        .new_reg("r1", 1)
+        .new_reg("r2", 1)
+        .new_reg("r3", 1)
         // Init random state
         .h(0)
         .h(1)
@@ -184,18 +184,38 @@ pub fn hybrid_test<
 }
 
 pub fn register_test<
-    D: BuildSimulator<HybridCircuit>
-        + DebuggableSimulator
-        + StoredCircuitSimulator
-        + HybridSimulator<Value>,
+    D: BuildSimulator<HybridCircuit> + DebuggableSimulator + StoredCircuitSimulator + HybridSimulator,
 >() {
-    let circuit = Circuit::new(2).new_reg("r0").x(1).measure_bit(1, ("r0", 0));
+    let circuit = Circuit::new(2)
+        .new_reg("r0", 1)
+        .x(1)
+        .measure_bit(1, ("r0", 0));
 
     let mut sim = D::build(circuit).unwrap();
     while sim.next() {}
 
-    assert_eq!(sim.registers()["r0"], Value::Int(1));
+    assert_eq!(sim.registers()["r0"].read(), 1);
 }
+
+pub fn test_measure_overwrites_with_zero<T>()
+where
+    T: BuildSimulator<HybridCircuit>
+        + DebuggableSimulator
+        + StoredCircuitSimulator
+        + HybridSimulator,
+{
+    let circuit = Circuit::new(2)
+        .new_reg("tmp", 1)
+        .x(0)
+        .measure_bit(0, ("tmp", 0))
+        .measure_bit(1, ("tmp", 0));
+
+    let mut sim = T::build(circuit).unwrap();
+    sim.cont();
+
+    assert_eq!(sim.register("tmp").read(), 0);
+}
+
 pub fn deep_ctrl_sub<
     D: BuildSimulator<HybridCircuit> + DebuggableSimulator + StoredCircuitSimulator,
 >() {
--- quasim/src/register_file.rs
+++ quasim/src/register_file.rs
@@ -1,32 +1,98 @@
 use std::fmt::Display;
 use std::{
-    collections::{HashMap, HashSet},
+    collections::HashMap,
     ops::{Index, IndexMut},
 };
 
+#[derive(Debug, Clone, thiserror::Error)]
+pub enum RegisterError {
+    #[error("tried to write to out of bounds bit {0}")]
+    WriteBitError(usize),
+    #[error("tried to write invalid value {0}, expected 0 or 1")]
+    WriteBitValueError(usize),
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct Register {
+    value: usize,
+    size: usize,
+}
+
+impl Register {
+    /// Expects size in range 0 to 64
+    pub fn new(size: usize) -> Self {
+        Self { value: 0, size }
+    }
+
+    /// Expects value as 0 or 1
+    pub fn write_bit(&mut self, bit: usize, value: usize) -> Result<usize, RegisterError> {
+        if bit >= self.size {
+            return Err(RegisterError::WriteBitError(bit));
+        }
+        if !(value == 0 || value == 1) {
+            return Err(RegisterError::WriteBitValueError(value));
+        }
+
+        // Reset bit pos
+        self.value &= !(1 << bit);
+        // Write new bit value
+        self.value |= value << bit;
+
+        Ok(self.value)
+    }
+
+    pub fn write(&mut self, value: usize) {
+        // No write mask needed if size of value matches reg size
+        if self.size == size_of_val(&self.value) * 8 {
+            self.value = value
+        } else {
+            // Write mask is 1's in writable bit positions
+            let write_mask = (1 << self.size) - 1;
+            self.value = value & write_mask;
+        }
+    }
+
+    pub fn read_bit(&self, bit: usize) -> usize {
+        (self.value >> bit) & 1
+    }
+
+    pub fn read(&self) -> usize {
+        self.value
+    }
+}
+
+impl Display for Register {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:0>width$b}", self.value, width = self.size)
+    }
+}
+
 #[derive(Debug, Clone, Default)]
 /// Map containing named registers
-pub struct RegisterFile<T>(HashMap<String, T>);
+pub struct RegisterFile(HashMap<String, Register>);
 
-impl<T> RegisterFile<T> {
-    pub fn get(&self, key: &str) -> Option<&T> {
+impl RegisterFile {
+    pub fn get(&self, key: &str) -> Option<&Register> {
         self.0.get(key)
     }
 }
 
-impl<T: Default> From<&HashSet<String>> for RegisterFile<T> {
-    fn from(value: &HashSet<String>) -> Self {
-        let map = value
-            .into_iter()
-            .map(|name| (name.clone(), T::default()))
-            .collect();
+impl From<&HashMap<String, usize>> for RegisterFile {
+    fn from(value: &HashMap<String, usize>) -> Self {
+        let mut reg_map = HashMap::new();
 
-        Self(map)
+        for (name, &size) in value {
+            let new_reg = Register::new(size);
+
+            reg_map.insert(name.clone(), new_reg);
+        }
+
+        Self(reg_map)
     }
 }
 
-impl<T> Index<&str> for RegisterFile<T> {
-    type Output = T;
+impl Index<&str> for RegisterFile {
+    type Output = Register;
 
     fn index(&self, index: &str) -> &Self::Output {
         self.0
@@ -34,7 +100,7 @@ impl<T> Index<&str> for RegisterFile<T> {
             .expect(&format!("Unknown register {}", index))
     }
 }
-impl<T> IndexMut<&str> for RegisterFile<T> {
+impl IndexMut<&str> for RegisterFile {
     fn index_mut(&mut self, index: &str) -> &mut Self::Output {
         self.0
             .get_mut(index)
@@ -42,16 +108,13 @@ impl<T> IndexMut<&str> for RegisterFile<T> {
     }
 }
 
-impl<T: Clone> From<&RegisterFile<T>> for HashMap<String, T> {
-    fn from(value: &RegisterFile<T>) -> Self {
+impl From<&RegisterFile> for HashMap<String, Register> {
+    fn from(value: &RegisterFile) -> Self {
         value.0.clone()
     }
 }
 
-impl<T> Display for RegisterFile<T>
-where
-    T: Display,
-{
+impl Display for RegisterFile {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let key_width = self.0.keys().map(|k| k.len()).max().unwrap_or(1);
 
