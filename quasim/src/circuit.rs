3b452b3770949a16f117eb76a4c5426dd1070afc
Emil Karlsson <emil.axel.karlsson@gmail.com>
2026-04-07 11:54:01 +0200

    133 phase oracle from classical oracle (#165)
    * Algebraic Normal Form for classical functions
    
    * Phase oracle builder in circuit.rs module
    
    * Fixed bug with phase oracle truth table size
    
    * Phase oracle example with an is_prime(...) function
    
    * cargo fmt
    
    * Made proper tests
    
    * Pass usize by value instead of reference
    
    * Technically more correct is_prime function
    
    * Specify qubits by slice instead of vec
    
    * Truth table size assertion
    
    * Rewrite for general oracles, not just phase oracles
    
    * cargo fmt +
    
    * Merge conflict cleanup
    
    * cargo fmt
    
    * Added `new_oracle` for constructing a standalone circuit
    
    * Assert that anf_coefs table size makes sense

--- quasim/src/circuit.rs
+++ quasim/src/circuit.rs
@@ -1,10 +1,12 @@
 use std::{collections::HashMap, f64::consts::PI};
 pub mod breakpoint;
+pub mod oracle;
 pub mod pc;
 
 use crate::{
     circuit::{
         breakpoint::{Breakpoint, BreakpointList, IEBreakpoint},
+        oracle::{append_oracle, fn_to_truth_table, truth_table_to_anf_coefs},
         pc::CircuitPc,
     },
     expr_dsl::{BitExpr, BoolExpr},
@@ -57,6 +59,19 @@ impl Circuit {
         };
 
         s.qft(&(0..n_qubits).collect::<Vec<_>>())
+    }
+
+    /// Creates a new circuit implementing a quantum oracle for a given classical function.
+    /// `input_qubits` are specified in order of least significance, for example [0,1,2,3,4].
+    pub fn new_oracle(
+        n_qubits: usize,
+        input_qubits: &[usize],
+        target: usize,
+        classic_fn: impl Fn(usize) -> bool,
+    ) -> Self {
+        let s = Self::new(n_qubits);
+
+        s.oracle(input_qubits, target, classic_fn)
     }
 
     pub fn from_qasm_file(file_name: &str) -> Result<Self, QASMParseError> {
@@ -394,6 +409,19 @@ impl<B: CircuitBehaviour> Circuit<B> {
             self = self.swap(targets[i], targets[n - 1 - i]);
         }
         self
+    }
+
+    /// Appends a circuit implementing a quantum oracle for a given classical function.
+    /// `input_qubits` are specified in order of least significance, for example [0,1,2,3,4].
+    pub fn oracle(
+        self,
+        input_qubits: &[usize],
+        target: usize,
+        classic_fn: impl Fn(usize) -> bool,
+    ) -> Self {
+        let truth_table = fn_to_truth_table(&classic_fn, input_qubits.len());
+        let anf_coefs = truth_table_to_anf_coefs(truth_table);
+        append_oracle(self, input_qubits, target, anf_coefs)
     }
 
     // Breakpoint
