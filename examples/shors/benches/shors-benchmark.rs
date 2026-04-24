2415f8380a3941cc98fc04f6bbed6e6a8d5841aa
Sebastian Sandstig <sebastian.sandstig@gmail.com>
2026-04-24 15:43:16 +0200

    shors benchmark made modular

--- examples/shors/benches/shors-benchmark.rs
+++ examples/shors/benches/shors-benchmark.rs
@@ -1,29 +1,70 @@
-use std::time::Duration;
-
+use divan::Bencher;
+use shors::circuit;
+extern crate quasim;
 use quasim::{
     circuit::HybridCircuit,
-    sampler::CircuitSampler,
+    cube_simulator::CubeSimulator,
+    fmm_simulator::FullMatMulSimulator,
+    product_state_simulator::ProductStateSimulator,
+    sampler::{CircuitSampler, Sampler},
     simulator::{Buildable, Sampleable},
     sv_simulator::StateVectorSimulator,
 };
-use shors::circuit;
+#[cfg(feature = "gpu")]
+use quasim::{cubecl::wgpu::WgpuRuntime, gpu_sv_simulator::GpuStateVectorSimulator};
 
-extern crate quasim;
+macro_rules! bench {
+    ($($feat:literal =>)? $name:ident, $sim:ty, $qubits:expr $(;)?) => {
+        $(#[cfg(feature = $feat)])?
+        #[divan::bench(
+            consts = $qubits
+        )]
+        fn $name<const N: usize>(bencher: Bencher) {
+            let mut sim = build::<$sim, N>();
+            bench(bencher, &mut sim);
+        }
+    };
+
+    (
+        $($ffeat:literal =>)? $first:ident, $fsim:ty, $fqubits:expr $(;
+            $($feat:literal =>)? $name:ident, $sim:ty, $qubits:expr
+        )+ $(;)?
+    ) => {
+        bench!($($ffeat =>)? $first, $fsim, $fqubits);
+        bench!($($($feat =>)? $name, $sim, $qubits);+);
+    };
+}
+
+const NS: &[usize] = &[0x3, 0x7, 0xF, 0x3F, 0x7F, 0xFF];
+const FMM_NS: &[usize] = &[0x3, 0x7];
+bench!(
+    state_vector_simulator, StateVectorSimulator, NS;
+    "gpu" => gpu_accelerated, GpuStateVectorSimulator<WgpuRuntime>, NS;
+    full_mat_mul_simulator, FullMatMulSimulator, FMM_NS;
+    product_state_simulator, ProductStateSimulator, NS;
+    cube_simulator, CubeSimulator, NS;
+);
 
 fn main() {
     divan::Divan::from_args().main();
 }
 
-#[divan::bench(
-    types = [StateVectorSimulator],
-    args = [0b11, 0b1111, 0b1111_1111],
-    sample_count = 10,
-    max_time = Duration::from_secs(1)
-)]
-fn shors<S>(n: usize)
+fn build<S, const N: usize>() -> S
 where
-    S: Buildable<HybridCircuit> + Sampleable<HybridCircuit>,
+    S: Buildable<HybridCircuit>,
 {
-    let a = n - 1;
-    S::sample_once(circuit(n, a), CircuitSampler).unwrap();
+    let a = N-1;
+    S::build(circuit(N, a)).unwrap()
+}
+
+fn bench<S>(bencher: Bencher, sim: &mut S)
+where
+    S: Sampleable<HybridCircuit>,
+{
+    bencher.bench_local(|| {
+        divan::black_box(CircuitSampler.sample({
+            sim.run();
+            sim
+        }));
+    });
 }
