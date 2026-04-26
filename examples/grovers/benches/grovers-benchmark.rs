fcfcfacb7bfa7f020bf0f8838a922e92ad410676
Sebastian Sandstig <sebastian.sandstig@gmail.com>
2026-04-26 14:44:57 +0200

    readd qubits to grovers

--- examples/grovers/benches/grovers-benchmark.rs
+++ examples/grovers/benches/grovers-benchmark.rs
@@ -43,8 +43,8 @@ macro_rules! bench {
     };
 }
 
-const QUBITS: &[usize] = &[2, 4, 6, 8];//, 10, 12, 14, 16];
-const FMM_QUBITS: &[usize] = &[2, 4, 6];//, 8, 10];
+const QUBITS: &[usize] = &[2, 4, 6, 8, 10, 12, 14, 16];
+const FMM_QUBITS: &[usize] = &[2, 4, 6, 8, 10];
 bench!(
     state_vector_simulator, StateVectorSimulator, QUBITS;
     "wgpu" => gpu_accelerated_wgpu, GpuStateVectorSimulator<WgpuRuntime>, QUBITS;
