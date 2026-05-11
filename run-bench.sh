52b6923b3187471134367703c93fd41b736c140e
Sebastian Sandstig <sebastian.sandstig@gmail.com>
2026-05-11 15:55:05 +0200

    instructions in run-bench.sh

--- run-bench.sh
+++ run-bench.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
+# USAGE:
+# > run-bench.sh <out>
+
+# BENCH PARAMETERS
+# Change `bench-parameters.json` to alter the qubits and arguments used for each benchmark and simulator
+# OR set the BENCHMARK_PARAMETERS environment variable to a path pointing at a JSON file.
+
 export DIVAN_SAMPLE_COUNT=10
 export DIVAN_SAMPLE_SIZE=10
+#export BENCHMARk_PARAMETERS="/path/to/json"
+#export BENCH_PARAMETERS_TAKE=<count> # only take the first <count> bench_parameters for each bench.
 cargo bench --bench mid-measure-all > $1
 cargo bench --bench mid-measurements >> $1
 cargo bench --bench num-gates >> $1
